aa7aafbdc14839c5bb83556176761b79f3384208
Jorgon29 <00016823@uca.edu.sv>
2025-11-17 18:18:37 -0600

    Axios agregado

--- src/modules/mqtt.module.ts
+++ src/modules/mqtt.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MqttService } from '../services/mqtt.service';
 import { ConfigModule } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [ConfigModule],
